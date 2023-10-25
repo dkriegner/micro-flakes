eec775a01da3f6da8d0c77aa5fa62cd09f0d0ea6
Jiří Zelenka <jirka-zelenka@centrum.cz>
2023-10-25 11:03:33 +0200

    improving

--- Detector/Script.py
+++ Detector/Script.py
@@ -1,12 +1,9 @@
-from PIL import Image
 from openpyxl import Workbook
-from openpyxl.drawing.image import Image as Xl
 import cv2
 import os
 import shutil
-import numpy as np
 from find_objects import find_objects
-from process_object import gamma_correct, change_contrast
+from process_object import process_object
 
 def take_webcam_image(filename):
     # This function takes a photo by webcam.
@@ -114,11 +111,6 @@ anything = find_objects(name, out1, min_size, sensitivity)  # write coordinates 
 print("The second iteration:")
 # Now, find objects from the first iteration in the same area in high resolution
 
-name2 = "object"  # name of photos of found objects
-
-pim = Image.open(f'output/org_gc.png')  # open corrected original photo
-org = pim.load()
-
 # Set area for finding object in high resolution
 candidates = []  # (x_min, x_max, y_min, y_max)
 for q in anything:
@@ -152,182 +144,9 @@ max_width = 0
 print("processed: ", end="")
 for q in range(1, len(candidates)):
     print(f"{q}, ", end="")
-    nw = Image.new('RGB', (candidates[q][1] - candidates[q][0] + 8, candidates[q][3] - candidates[q][2] + 8),
-                   color='white')  # vytvoreni noveho obrazku
-    new = nw.load()
-    ts = Image.new('RGB', (candidates[q][1] - candidates[q][0] + 8, candidates[q][3] - candidates[q][2] + 8),
-                   color='white')  # vytvoreni noveho obrazku
-    test = ts.load()
-
-    detect2 = []
-    bright2 = 0
-
-    for i in range(candidates[q][0] - 4, candidates[q][1] + 4):
-        for j in range(candidates[q][2] - 4, candidates[q][3] + 4):
-            R, G, B = org[i, j]
-            new[i - candidates[q][0] - 1 + 5, j - candidates[q][2] - 1 + 5] = org[i, j]
-            bright2 += R + G + B
-            if R > 80 or G > 80 or B > 80:
-                org[i, j] = (256, 0, 0)
-    nw.save(f'output/objects/{name2}_{q}.png')  # storage image q-th objects
-
-    for i in range(candidates[q][0] - 4, candidates[q][1] - 1 + 4, 2):
-        for j in range(candidates[q][2] - 4, candidates[q][3] - 1 + 4, 2):
-            red = 0
-            px = 0
-            for k in range(i - 1, i + 1):
-                for l in range(j - 1, j + 1):
-                    if org[k, l] == (255, 0, 0):
-                        red += 1
-                    px += 1
-            if px != 0:
-                if red / float(px) > 0.6:
-                    detect2.append((i, j))
-
-    detect2 = np.array(detect2)
-
-    anything2 = []
-
-    i = 0
-    while len(detect2) > 0:
-        seed = detect2[0]
-        queue = [seed]
-        detect2 = np.delete(detect2, 0, axis=0)
-
-        b = 1
-        while b == 1:
-            b = 0
-            for (x, y) in detect2:
-                dx_values = [2, -2, 0, 0, 2, -2, 2, -2]
-                dy_values = [0, 0, 2, -2, 2, -2, -2, 2]
-
-                for dx, dy in zip(dx_values, dy_values):
-                    neighbor = (x + dx, y + dy)
-                    mask = np.all(detect2 == neighbor, axis=1)
-                    if np.any(mask):
-                        queue.append(neighbor)
-                        detect2 = detect2[~mask]
-                        b = 1
-
-        anything2.append(queue)
-        i += 1
-
-    j = 0
-    for a in range(len(anything2)):
-        if len(anything2[j]) <= min_size:
-            anything2.remove(anything2[j])
-            j -= 1
-        j += 1
-
-    index = 0
-    if len(anything2) == 1:
-        for n in range(len(anything2)):
-            for (i, j) in anything2[n]:
-                for k in range(i - 1, i + 1):
-                    for l in range(j - 1, j + 1):
-                        test[k - candidates[q][0] - 1 + 5, l - candidates[q][2] - 1 + 5] = (256, 0, 0)
-    elif len(anything2) == 0:
-        continue
-    else:
-        mx = max(len(x) for x in anything2)
-        for n in range(len(anything2)):
-            if len(anything2[n]) == mx:
-                index = n
-                for (i, j) in anything2[n]:
-                    for k in range(i - 1, i + 1):
-                        for l in range(j - 1, j + 1):
-                            test[k - candidates[q][0] - 1 + 5, l - candidates[q][2] - 1 + 5] = (256, 0, 0)
-
-    centre = ((int(sum(x for (x, y) in anything2[index]) / len(anything2[index])),
-               int(sum(y for (x, y) in anything2[index]) / len(anything2[index]))))
-
-    # mark shapes of image
-    shape = Image.open(f'output/objects/{name2}_{q}.png')
-    shape = gamma_correct(shape, 1.5)
-    shape = change_contrast(shape, 100)
-    shape.save(f'output/objects/processing0_{q}.png')
-
-    shape = cv2.imread(f'output/objects/processing0_{q}.png')
-    shape = cv2.cvtColor(shape, cv2.COLOR_BGR2RGB)
-    shape = cv2.addWeighted(shape, 4, shape, 0, 0)
-    shape = cv2.cvtColor(shape, cv2.COLOR_BGR2GRAY)
-    shape = cv2.Canny(shape, 300, 100)
-    cv2.imwrite(f'output/objects/processing_{q}.png', shape)
-
-    prc = Image.open(f'output/objects/processing_{q}.png')  # open corrected original photo
-    pc = prc.load()
-
-    # calculate size of the object (new)
-    size = 0
-    full_size = 0
-
-    x_min = x_max = 0
-    for i in range(prc.size[0]):
-        white = 0
-        for j in range(prc.size[1]):
-            W = pc[i, j]
-            if W == 255 and white == 0:
-                white = 1
-                size += 1
-                x_min = j
-            elif W == 255 and white == 1:
-                size += 1
-                x_max = j
-        full_size += (x_max - x_min)
-        x_min = x_max = 0
-
-    # calculate size of the object (old)
-    size2 = 0
-    full_size2 = 0
-
-    x_min = x_max = 0
-    for i in range(ts.size[0] - 3):
-        red = 0
-        for j in range(ts.size[1] - 3):
-            R, G, B = test[i, j]
-            if (R, G, B) == (255, 0, 0) and red == 0:
-                red = 1
-                size2 += 1
-                x_min = j
-            elif (R, G, B) == (255, 0, 0) and red == 1:
-                size2 += 1
-                x_max = j
-        full_size2 += (x_max - x_min)
-        x_min = x_max = 0
-
-    ts.save(f'output/objects/{name2}2_{q}.png')  # store photo of area of detect object
-
-    # fill Excel table
-    img = Xl(f'output/objects/{name2}_{q}.png')
-    sheet.add_image(img, f'G{q + 1}')
-    img2 = Image.open(f'output/objects/{name2}_{q}.png')
-    sheet.row_dimensions[q + 1].height = img2.height * 0.8
-    if max_width < img2.width:
-        max_width = img2.width
-    sheet[f"A{q + 1}"] = q
-    sheet[f"B{q + 1}"] = centre[0] * calibration
-    sheet[f"C{q + 1}"] = centre[1] * calibration
-    sheet[f"D{q + 1}"] = full_size2 * calibration ** 2
-    sheet[f"E{q + 1}"] = 1 - size2 / full_size2
-    sheet[f"F{q + 1}"] = max(((-size2 - (size2 ** 2 - 16 * full_size2) ** 0.5) / 4) / (
-                4 * full_size2 / (-size2 - (size2 ** 2 - 16 * full_size2) ** 0.5)),
-                             (4 * full_size2 / (-size2 - (size2 ** 2 - 16 * full_size2) ** 0.5)) / (
-                                         (-size2 - (size2 ** 2 - 16 * full_size2) ** 0.5) / 4))
-    sheet[f"H{q + 1}"] = size
-    sheet[f"I{q + 1}"] = full_size
-    if 3.5 < full_size / size < 5:
-        sheet[f"J{q + 1}"] = "OK"
-        sheet[f"K{q + 1}"] = abs(full_size / size - (3.5 + 5) / 2)
-    else:
-        sheet[f"J{q + 1}"] = "NO"
-    if 0.08 < 1 - size2 / full_size2:
-        sheet[f"L{q + 1}"] = "OK"
-        sheet[f"M{q + 1}"] = abs(1 - size2 / full_size2) - 0.08
-    else:
-        sheet[f"L{q + 1}"] = "NO"
-
-    sheet[f"N{q + 1}"] = bright2 / size2
-    sheet[f"O{q + 1}"] = 20 * bright2 / size2 - 6940
+    width = process_object(candidates[q], min_size, q, calibration, workbook)
+    if max_width < width:
+        max_width = width
 
 sheet.column_dimensions['G'].width = max_width * 0.15
 workbook.save("output/Index_of_objects.xlsx")  # save Excel table
--- Detector/process_object.py
+++ Detector/process_object.py
@@ -1,5 +1,7 @@
 from PIL import Image
-
+import numpy as np
+from openpyxl.drawing.image import Image as Xl
+import cv2
 
 def gamma_correct(im, gamma):
     gamma1 = gamma
@@ -21,3 +23,189 @@ def change_contrast(img, level):
     def contrast(c):
         return 128 + factor * (c - 128)
     return img.point(contrast)
+
+
+def process_object(coordinates, min_size, q, calibration, workbook):
+    name2 = "object"  # name of photos of found objects
+
+    pim = Image.open(f'output/org_gc.png')  # open corrected original photo
+    org = pim.load()
+
+    nw = Image.new('RGB', (coordinates[1] - coordinates[0] + 8, coordinates[3] - coordinates[2] + 8),
+                   color='white')  # vytvoreni noveho obrazku
+    new = nw.load()
+    ts = Image.new('RGB', (coordinates[1] - coordinates[0] + 8, coordinates[3] - coordinates[2] + 8),
+                   color='white')  # vytvoreni noveho obrazku
+    test = ts.load()
+
+    detect2 = []
+    bright2 = 0
+
+    for i in range(coordinates[0] - 4, coordinates[1] + 4):
+        for j in range(coordinates[2] - 4, coordinates[3] + 4):
+            R, G, B = org[i, j]
+            new[i - coordinates[0] - 1 + 5, j - coordinates[2] - 1 + 5] = org[i, j]
+            bright2 += R + G + B
+            if R > 80 or G > 80 or B > 80:
+                org[i, j] = (256, 0, 0)
+    nw.save(f'output/objects/{name2}_{q}.png')  # storage image q-th objects
+
+    for i in range(coordinates[0] - 4, coordinates[1] - 1 + 4, 2):
+        for j in range(coordinates[2] - 4, coordinates[3] - 1 + 4, 2):
+            red = 0
+            px = 0
+            for k in range(i - 1, i + 1):
+                for l in range(j - 1, j + 1):
+                    if org[k, l] == (255, 0, 0):
+                        red += 1
+                    px += 1
+            if px != 0:
+                if red / float(px) > 0.6:
+                    detect2.append((i, j))
+
+    detect2 = np.array(detect2)
+
+    anything2 = []
+
+    i = 0
+    while len(detect2) > 0:
+        seed = detect2[0]
+        queue = [seed]
+        detect2 = np.delete(detect2, 0, axis=0)
+
+        b = 1
+        while b == 1:
+            b = 0
+            for (x, y) in detect2:
+                dx_values = [2, -2, 0, 0, 2, -2, 2, -2]
+                dy_values = [0, 0, 2, -2, 2, -2, -2, 2]
+
+                for dx, dy in zip(dx_values, dy_values):
+                    neighbor = (x + dx, y + dy)
+                    mask = np.all(detect2 == neighbor, axis=1)
+                    if np.any(mask):
+                        queue.append(neighbor)
+                        detect2 = detect2[~mask]
+                        b = 1
+
+        anything2.append(queue)
+        i += 1
+
+    j = 0
+    for a in range(len(anything2)):
+        if len(anything2[j]) <= min_size:
+            anything2.remove(anything2[j])
+            j -= 1
+        j += 1
+
+    index = 0
+    if len(anything2) == 1:
+        for n in range(len(anything2)):
+            for (i, j) in anything2[n]:
+                for k in range(i - 1, i + 1):
+                    for l in range(j - 1, j + 1):
+                        test[k - coordinates[0] - 1 + 5, l - coordinates[2] - 1 + 5] = (256, 0, 0)
+    elif len(anything2) == 0:
+        pass
+    else:
+        mx = max(len(x) for x in anything2)
+        for n in range(len(anything2)):
+            if len(anything2[n]) == mx:
+                index = n
+                for (i, j) in anything2[n]:
+                    for k in range(i - 1, i + 1):
+                        for l in range(j - 1, j + 1):
+                            test[k - coordinates[0] - 1 + 5, l - coordinates[2] - 1 + 5] = (256, 0, 0)
+
+    centre = ((int(sum(x for (x, y) in anything2[index]) / len(anything2[index])),
+               int(sum(y for (x, y) in anything2[index]) / len(anything2[index]))))
+
+    # mark shapes of image
+    shape = Image.open(f'output/objects/{name2}_{q}.png')
+    shape = gamma_correct(shape, 1.5)
+    shape = change_contrast(shape, 100)
+    shape.save(f'output/objects/processing0_{q}.png')
+
+    shape = cv2.imread(f'output/objects/processing0_{q}.png')
+    shape = cv2.cvtColor(shape, cv2.COLOR_BGR2RGB)
+    shape = cv2.addWeighted(shape, 4, shape, 0, 0)
+    shape = cv2.cvtColor(shape, cv2.COLOR_BGR2GRAY)
+    shape = cv2.Canny(shape, 300, 100)
+    cv2.imwrite(f'output/objects/processing_{q}.png', shape)
+
+    prc = Image.open(f'output/objects/processing_{q}.png')  # open corrected original photo
+    pc = prc.load()
+
+    # calculate size of the object (new)
+    size = 0
+    full_size = 0
+
+    x_min = x_max = 0
+    for i in range(prc.size[0]):
+        white = 0
+        for j in range(prc.size[1]):
+            W = pc[i, j]
+            if W == 255 and white == 0:
+                white = 1
+                size += 1
+                x_min = j
+            elif W == 255 and white == 1:
+                size += 1
+                x_max = j
+        full_size += (x_max - x_min)
+        x_min = x_max = 0
+
+    # calculate size of the object (old)
+    size2 = 0
+    full_size2 = 0
+
+    x_min = x_max = 0
+    for i in range(ts.size[0] - 3):
+        red = 0
+        for j in range(ts.size[1] - 3):
+            R, G, B = test[i, j]
+            if (R, G, B) == (255, 0, 0) and red == 0:
+                red = 1
+                size2 += 1
+                x_min = j
+            elif (R, G, B) == (255, 0, 0) and red == 1:
+                size2 += 1
+                x_max = j
+        full_size2 += (x_max - x_min)
+        x_min = x_max = 0
+
+    ts.save(f'output/objects/{name2}2_{q}.png')  # store photo of area of detect object
+
+    # fill Excel table
+    sheet = workbook.active
+    img = Xl(f'output/objects/{name2}_{q}.png')
+    sheet.add_image(img, f'G{q + 1}')
+    img2 = Image.open(f'output/objects/{name2}_{q}.png')
+    sheet.row_dimensions[q + 1].height = img2.height * 0.8
+
+    sheet[f"A{q + 1}"] = q
+    sheet[f"B{q + 1}"] = centre[0] * calibration
+    sheet[f"C{q + 1}"] = centre[1] * calibration
+    sheet[f"D{q + 1}"] = full_size2 * calibration ** 2
+    sheet[f"E{q + 1}"] = 1 - size2 / full_size2
+    sheet[f"F{q + 1}"] = max(((-size2 - (size2 ** 2 - 16 * full_size2) ** 0.5) / 4) / (
+                4 * full_size2 / (-size2 - (size2 ** 2 - 16 * full_size2) ** 0.5)),
+                             (4 * full_size2 / (-size2 - (size2 ** 2 - 16 * full_size2) ** 0.5)) / (
+                                         (-size2 - (size2 ** 2 - 16 * full_size2) ** 0.5) / 4))
+    sheet[f"H{q + 1}"] = size
+    sheet[f"I{q + 1}"] = full_size
+    if 3.5 < full_size / size < 5:
+        sheet[f"J{q + 1}"] = "OK"
+        sheet[f"K{q + 1}"] = abs(full_size / size - (3.5 + 5) / 2)
+    else:
+        sheet[f"J{q + 1}"] = "NO"
+    if 0.08 < 1 - size2 / full_size2:
+        sheet[f"L{q + 1}"] = "OK"
+        sheet[f"M{q + 1}"] = abs(1 - size2 / full_size2) - 0.08
+    else:
+        sheet[f"L{q + 1}"] = "NO"
+
+    sheet[f"N{q + 1}"] = bright2 / size2
+    sheet[f"O{q + 1}"] = 20 * bright2 / size2 - 6940
+
+    return img2.width
